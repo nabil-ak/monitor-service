eaac8d481ae7cdae9b0530719ae256793e5f317a
Nabil <nabil.akir@outlook.de>
2022-04-25 00:32:40 +0200

    debug

--- monitors/wethenew.py
+++ monitors/wethenew.py
@@ -86,7 +86,7 @@ class wethenew:
         #Get all Products from the Site
         while True:
             response = self.scraper.get(f'https://sell.wethenew.com/api/products?skip={skip}&take=100&onlyWanted=true', proxies=proxy)
-            print(response.text)
+            logging.info(msg=f'[wethenew] {response.text}')
             response.raise_for_status()
             r = response.json()
             for product in r["results"]:
