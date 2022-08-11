aea181fbddcf36544e60e2711bb3e200a7e0fa5c
Nabil <nabil.akir@outlook.de>
2022-08-11 13:46:46 +0200

    updated user_agent

--- user_agent.py
+++ user_agent.py
@@ -5,7 +5,7 @@ import time
 import traceback
 import database
 
-USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
 
 def getcurrentChromeUseragent():
     """
