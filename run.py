8eb4bae3b3c860bf589cc16b8ef8e4a694cf23b5
Nabil <nabil.akir@outlook.de>
2022-09-18 15:30:04 +0200

    added us region to the asos monitor

--- run.py
+++ run.py
@@ -61,7 +61,7 @@ def startMonitors():
     chrome_user_agent = getcurrentChromeUseragent()
 
     #Create all Asos Monitors
-    ASOSREGIONS = [["de","eur"], ["fr","eur"], ["es","eur"] , ["it","eur"], ["com","gbp"], ["roe","eur"]]
+    ASOSREGIONS = [["de","eur"], ["fr","eur"], ["es","eur"] , ["it","eur"], ["com","gbp"], ["roe","eur"], ["us","usd"]]
     for region in ASOSREGIONS:
         a = asos.asos(groups=cookgroups,skus=settings["asos"]["skus"],delay=settings["asos"]["delay"],region=region[0],currency=region[1],user_agents=user_agents,proxys=proxys)
         monitorPool.append(Process(target=a.monitor))
