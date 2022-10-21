7c28757970b081d7590ab11a816c95b18de234ed
Nabil <nabil.akir@outlook.de>
2022-10-21 14:24:51 +0200

    added headers to prodirectsoccer monitor

--- monitors/prodirectsoccer.py
+++ monitors/prodirectsoccer.py
@@ -126,9 +126,19 @@ class prodirectsoccer:
         # Ensures that first scrape does not notify all products
         start = 1
 
-        # Initialising headers
+        # Initialising headers    
         headers = {
-                'user-agent': self.user_agent
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
+            'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': self.user_agent,
         }
         
         while True:
