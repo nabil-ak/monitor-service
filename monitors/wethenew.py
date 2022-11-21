b961194e7a124761ad4e18b62b9d25bb104b4317
Nabil <nabil.akir@outlook.de>
2022-11-21 16:27:57 +0100

    set proxys for wethenew back to packet and decreased the retry delay for wethenew back to 5s

--- monitors/wethenew.py
+++ monitors/wethenew.py
@@ -253,7 +253,7 @@ class wethenew:
             except Exception as e:
                 print(f"[wethenew-{self.endpoint}] Exception found: {traceback.format_exc()}")
                 logging.error(e)
-                time.sleep(10)
+                time.sleep(5)
 
 
 if __name__ == '__main__':
