da9fad3d97b4d01a506e5c9025ffa4428285e9d7
Nabil Akir <nabil.akir@outlook.de>
2023-03-22 22:15:35 +0100

    revert debug commits

--- run.py
+++ run.py
@@ -138,6 +138,4 @@ if __name__ == "__main__":
     startMonitors()
 
     #Check if new Group was added or updated and also check if settings was updated
-    #Thread(target=updateData).start()
-    while True:
-        time.sleep(2)
+    Thread(target=updateData).start()
